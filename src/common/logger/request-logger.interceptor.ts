7c5b1e9254d3258ec5a4dde101b6d424e4160edb
ginans <norambuenas.gina@gmail.com>
2024-12-26 12:36:44 -0300

    Feature: Sistema de logging de peticiones al sistema

---
+++ src/common/logger/request-logger.interceptor.ts
@@ -0,0 +1,54 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import * as winston from 'winston';
+import { winstonConfig } from './winston.config';
+
+@Injectable()
+export class RequestLoggerInterceptor implements NestInterceptor {
+  private logger: winston.Logger;
+
+  constructor() {
+    this.logger = winston.createLogger(winstonConfig);
+  }
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    const { method, url, body, headers } = request;
+
+    const logData = {
+      timestamp: new Date().toISOString(),
+      method,
+      url,
+      body,
+      headers,
+    };
+
+    this.logger.info('Incoming Request', logData);
+
+    const now = Date.now();
+    return next.handle().pipe(
+      tap({
+        next: (responseBody) => {
+          this.logger.info('Request Completed', {
+            ...logData,
+            responseTime: `${Date.now() - now}ms`,
+            body: responseBody, // This will log the response body
+          });
+        },
+        error: (err) => {
+          this.logger.error('Request Failed', {
+            ...logData,
+            responseTime: `${Date.now() - now}ms`,
+            error: err.message,
+          });
+        },
+      }),
+    );
+  }
+}

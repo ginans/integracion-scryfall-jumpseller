90095ab9558f31eb859646d45480d2fe7ff44fe9
ginans <norambuenas.gina@gmail.com>
2025-02-21 17:01:46 -0300

    Agregar guardia de roles y decorador para controlar el acceso a las rutas de usuarios

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,31 +11,38 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UserRole } from './enums/user-role.enum';
 
 @Controller('users')
-// @UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+  
   @ApiBearerAuth()
   @ApiOperation({ summary: 'Obtener lista de usuarios' })
   @ApiResponse({
     status: 200,
     description: 'Operación exitosa',
   })
+  @Roles(UserRole.Admin, UserRole.User)
   @Get()
   getUsers(): Promise<User[]> {
     return this.usersService.findAll();
   }
+  
   @ApiBearerAuth()
   @ApiOperation({ summary: 'Obtener usuario por id' })
   @ApiResponse({
     status: 200,
     description: 'Operación exitosa',
   })
+  @Roles(UserRole.Admin, UserRole.User)
   @Get(':id')
   getUser(@Param('id') id: string): Promise<User | null> {
     return this.usersService.findById(id);
@@ -44,7 +51,7 @@ export class UsersController {
   @ApiBearerAuth()
   @ApiOperation({ summary: 'crear un nuevo usuario' })
   @Post()
-  @UseGuards(JwtAuthGuard)
+  @Roles(UserRole.Admin)
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.createUser(createUserDto);
   }
@@ -55,6 +62,7 @@ export class UsersController {
     status: 201,
     description: 'Operación exitosa',
   })
+  @Roles(UserRole.Admin)
   @Put(':id')
   updateUser(
     @Param('id') id: string,
@@ -62,22 +70,26 @@ export class UsersController {
   ): Promise<User | null> {
     return this.usersService.update(id, user);
   }
+  
   @ApiBearerAuth()
   @ApiOperation({ summary: 'desactivar usuario por id' })
   @ApiResponse({
     status: 201,
     description: 'Operación exitosa',
   })
+  @Roles(UserRole.Admin)
   @Patch(':id/isActive')
   updateUserIsActive(@Param('id') id: string): Promise<User | null> {
     return this.usersService.updateIsActive(id);
   }
+  
   @ApiBearerAuth()
   @ApiOperation({ summary: 'eliminar usuario por id' })
   @ApiResponse({
     status: 201,
     description: 'Operación exitosa',
   })
+  @Roles(UserRole.Admin)
   @Delete(':id')
   deleteUser(@Param('id') id: string) {
     return this.usersService.deleteById(id);
